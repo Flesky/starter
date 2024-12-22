f56ef65acc6f77a72acf0320dee0fca5be08aae7
Baron <49512397+Flesky@users.noreply.github.com>
2024-12-23 03:15:00 +0800

    Add auth middleware

--- server/lib/db.ts
+++ server/lib/db.ts
@@ -2,6 +2,6 @@ import { env } from 'node:process'
 import { drizzle } from 'drizzle-orm/libsql'
 
 export const db = drizzle({ connection: {
-  url: env.DATABASE_URL as string,
-  authToken: env.DATABASE_AUTH_TOKEN as string,
+  url: env.DATABASE_URL!,
+  authToken: env.DATABASE_AUTH_TOKEN!,
 } })
