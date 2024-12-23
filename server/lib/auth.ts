72ef9c3006233770c7f26c27669c2e8b86d3f3cc
Baron <49512397+Flesky@users.noreply.github.com>
2024-12-24 02:22:29 +0800

    Move db declaration

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,8 +1,8 @@
 import { env } from 'node:process'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '../db'
 import { account, session, user, verification } from '../db/schema'
-import { db } from './db'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
